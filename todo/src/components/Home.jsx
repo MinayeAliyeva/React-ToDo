4163fd57ae334c44437707c7652ff97315daa34e
MinayeAliyeva <7s346pu@code.edu.az>
2023-12-01 01:46:01 +0400

    changes

--- todo/src/components/Home.jsx
+++ todo/src/components/Home.jsx
@@ -1,14 +1,38 @@
-import React from 'react'
-import Header from '../layout/Header'
+import React, { useState } from "react";
 
 const Home = () => {
+  const initialState = {
+    name: "",
+    age: 0,
+  };
+  const [user, setUser] = useState(initialState);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser((prevs) => ({
+      ...prevs,
+      [name]: value,
+    }));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+  };
 
   return (
-    <div>
+    <form onSubmit={(e) => handleSubmit(e)}>
+      <label>Name</label>
+      <input value={user.name} onChange={(e) => handleChange(e)} name="name" />
+      <label>Age</label>
+      <input
+        value={user.age}
+        onChange={(e) => handleChange(e)}
+        name="age"
+        type="number"
+      />
+      <button>Add</button>
+    </form>
+  );
+};
 
-           
-    </div>
-  )
-}
-
-export default Home
+export default Home;
--- todo/src/index.js
+++ todo/src/index.js
@@ -10,11 +10,11 @@ const router = createBrowserRouter(routes);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+
     <RouterProvider router={router}>
       <App />
     </RouterProvider>
-  </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
